1d1d2e73b47c79117989c98fca3e88e182aad906
Mussels Ismene <ishamil86@gmail.com>
2021-04-19 17:21:57 +0300

    EXIT handle

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream> // std::cout
-#include <stdarg.h> //scanf
+#include <stdarg.h> //scanf()
+#include <string.h> // strcmp()
 
 int main(void)
 {
-	char	*string;
+	char	string[80];
 
 	scanf("%s", string);
-	std::cout << string << std::endl;
+	if (!strcmp(string, "EXIT"))
+	{
+		return 0;
+	}
+	else
+	{
+		std::cout << "hello world" << std::endl;
+	}
+	return 0;
 }
