c3005dbe508e8b3bf610d8fca4b444189f867658
Mussels Ismene <ishamil86@gmail.com>
2021-05-22 16:42:43 +0300

    init files 0403

---
+++ module04/ex03/AMateria.cpp
@@ -0,0 +1,18 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const & type): _type(type), _xp(0) {}
+AMateria::AMateria(const AMateria & other) {
+	*this = other;
+}
+
+AMateria& AMateria::operator=(const AMateria & other) {
+	_type = other._type;
+	_xp = other._xp;
+	return *this;
+}
+
+AMateria::~AMateria() {}
+
+std::string	const & AMateria::getType() const {return _type;}
+unsigned int AMateria::getXP() const {return _xp;}
+
---
+++ module04/ex03/AMateria.hpp
@@ -0,0 +1,25 @@
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <string>
+#include <iostream>
+
+class AMateria {
+	private:
+		std::string		_type;
+		unsigned int	_xp;
+
+	public:
+		AMateria(std::string const & type);
+		AMateria(const AMateria & other);
+		AMateria& operator=(const AMateria & other);
+		virtual ~AMateria();
+
+		std::string const & getType() const;
+		unsigned int	getXP() const;
+
+		// virtual AMateria* clone() const = 0;
+		// virtual	void use(ICharacter& target);
+};
+
+#endif
