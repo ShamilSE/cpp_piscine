ca739d3c9273de280bdc4588536b4eed0e10b8bd
Mussels Ismene <ishamil86@gmail.com>
2021-05-30 22:49:47 +0300

    0802 almost done

--- module08/ex02/mutantstack.hpp
+++ module08/ex02/mutantstack.hpp
@@ -4,13 +4,28 @@
 #include <string>
 #include <iostream>
 #include <stack>
+#include <list>
+#include <vector>
 
-// template <typename T>
-// class	MutantStack : public std::stack {
-// 	private:
-// 		T*	_data;
+template <typename T>
+class	MutantStack : public std::stack<T> {
+	public:
+		MutantStack() {
+			std::cout << "constructor called" << std::endl;
+		}
+		MutantStack(const MutantStack & other) {
+			*this = other;
+		}
+		MutantStack& operator=(const MutantStack & other) {
+			std::cout << "copy constructor called" << std::endl;
+			(void)other;
+			return *this;
+		}
+		~MutantStack() {};
 
-	
-// };
+	 	typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator	begin() {return (std::stack<T>::c.begin());}
+		iterator	end() {return (std::stack<T>::c.end());}	
+};
 
 #endif
