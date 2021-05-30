584d1a031c2b12a7ab686196f23f97c7a3345cc2
Mussels Ismene <ishamil86@gmail.com>
2021-05-30 11:36:06 +0300

    add documentation

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -5,18 +5,32 @@
 #include <vector>
 #include <exception>
 
-class	ValueNotFoundException : public std::exception {
+/**
+* @brief ValueNotFound
+*/
+class	ValueNotFound : public std::exception {
 	const char* what() const throw() {
 		return "value not found";
 	}
 };
 
+
+/**
+ * @brief Get the first occurence of an integer in
+ * a integer container.
+ * 
+ * @param a is the integer container.
+ * @param b is the integer to found.
+ * 
+ * @throw a ValueNotFound exception if the integer is not
+ * in the container.
+ */
 template <typename T>
 std::vector<int>::const_iterator	easyfind(T& a, int b) {
 	// const iterator of vector type int
 	std::vector<int>::const_iterator it = std::find(a.begin(), a.end(), b);
 	if (it == a.end())
-		throw ValueNotFoundException();
+		throw ValueNotFound();
 	return it;
 }
 
