556f4bf6aea3c9952bdf91143497612954b470c4
Mussels Ismene <ishamil86@gmail.com>
2021-05-30 17:07:38 +0300

    0802 init

---
+++ module08/ex02/main.cpp
@@ -0,0 +1,5 @@
+#include "mutantstack.hpp"
+
+int	main(void) {
+	return 0;
+}
---
+++ module08/ex02/mutantstack.hpp
@@ -0,0 +1,16 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <string>
+#include <iostream>
+#include <stack>
+
+// template <typename T>
+// class	MutantStack : public std::stack {
+// 	private:
+// 		T*	_data;
+
+	
+// };
+
+#endif
